d4b88699bfb5464bf0bc889db04d71513ed3e731
Laurent Baumann <git@lobau.io>
2023-11-09 14:24:50 -0800

    fixes

---
+++ server.py
@@ -0,0 +1,16 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import ssl
+from pathlib import Path
+
+port = 4443
+
+httpd = HTTPServer(("localhost", port), SimpleHTTPRequestHandler)
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain(Path(__file__).parent / "server.pem")
+httpd.socket = ssl_context.wrap_socket(
+    httpd.socket,
+    server_side=True,
+)
+
+print(f"Serving on https://localhost:{port}")
+httpd.serve_forever()
